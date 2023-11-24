71c57e81061827dd691f292ab924a63e302642a1
fei-ke <jinyang656@gmail.com>
2023-11-24 18:26:37 +0800

    Handle certs in two locations

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,16 +1,39 @@
 MODDIR=${0%/*}
 
-mkdir -p $MODDIR/cacerts
-rm $MODDIR/system/etc/security/cacerts/*
+set_perm() {
+  chown $2:$3 $1 || return 1
+  chmod $4 $1 || return 1
+  local CON=$5
+  [ -z $CON ] && CON=u:object_r:system_file:s0
+  chcon $CON $1 || return 1
+}
 
-if [ -d /apex/com.android.conscrypt/cacerts ] && [ "$(getprop system.certs.enabled)" != "true" ] && [ "$(getprop ro.build.version.sdk)" -ge 34 ]; then
-	cp -f /apex/com.android.conscrypt/cacerts/* $MODDIR/cacerts/
-	cp -f /data/misc/user/0/cacerts-added/* $MODDIR/cacerts/
-	set_perm_recursive $MODDIR/system/etc/security/cacerts/ root shell 644 644 u:object_r:system_security_cacerts_file:s0
-	mount --bind $MODDIR/cacerts/ /apex/com.android.conscrypt/cacerts/
-else
-	cp -f /system/etc/security/cacerts/* $MODDIR/cacerts/
-	cp -f /data/misc/user/0/cacerts-added/* $MODDIR/cacerts/
-	set_perm_recursive $MODDIR/system/etc/security/cacerts/ root root 644 644 u:object_r:system_security_cacerts_file:s0
-	mount --bind $MODDIR/cacerts/ /system/etc/security/cacerts/
+set_perm_recursive() {
+  find $1 -type d 2>/dev/null | while read dir; do
+    set_perm $dir $2 $3 $4 $6
+  done
+  find $1 -type f -o -type l 2>/dev/null | while read file; do
+    set_perm $file $2 $3 $5 $6
+  done
+}
+
+
+if [ -d /apex/com.android.conscrypt/cacerts ]; then
+	tmp_cacerts_dir=$MODDIR/conscrypt_cacerts
+	mkdir -p $tmp_cacerts_dir
+	rm $tmp_cacerts_dir/*
+	cp -f /apex/com.android.conscrypt/cacerts/* $tmp_cacerts_dir/
+	cp -f /data/misc/user/0/cacerts-added/* $tmp_cacerts_dir/
+	set_perm_recursive $tmp_cacerts_dir root shell 755 644 u:object_r:system_security_cacerts_file:s0
+	mount --bind $tmp_cacerts_dir /apex/com.android.conscrypt/cacerts
+fi
+
+if [ -d /system/etc/security/cacerts ]; then
+	tmp_cacerts_dir=$MODDIR/etc_cacerts
+	mkdir -p $tmp_cacerts_dir
+	rm $tmp_cacerts_dir/*
+	cp -f /system/etc/security/cacerts/* $tmp_cacerts_dir/
+	cp -f /data/misc/user/0/cacerts-added/* $tmp_cacerts_dir/
+	set_perm_recursive $tmp_cacerts_dir root root 755 644 u:object_r:system_security_cacerts_file:s0
+	mount --bind $tmp_cacerts_dir /system/etc/security/cacerts
 fi
